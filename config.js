8b696f852dd0051ce251c771920f1c532da2b510
tyrrellcheong <70849085+tyrrellcheong@users.noreply.github.com>
2020-09-06 21:53:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "Click for Start",
+        play: "Click for Play Music",
+        bannar_coming: "Add some decoration?",
+        balloons_flying: "Feel like missing something?",
+        cake_fadein: "Date？",
+        light_candle: "Candle？",
+        wish_message: "HAPPY BIRTHDAY",
+        story: "A SPECIAL MESSAGE FOR YOU",
     }
 };
